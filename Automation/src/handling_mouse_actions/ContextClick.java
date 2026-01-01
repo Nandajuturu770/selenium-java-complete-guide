e0fb2e70cdd3880fdf6a7f0abf371264f8bc6fae
Nandakiran <nandajuturu@gmail.com>
2026-01-01 12:13:48 +0530

    context class has been updated

--- Automation/src/handling_mouse_actions/ContextClick.java
+++ Automation/src/handling_mouse_actions/ContextClick.java
@@ -1,5 +1,56 @@
 package handling_mouse_actions;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
 public class ContextClick {
 
+    private static WebDriver driver;
+    private static ChromeOptions options;
+    private static Actions actions;
+    private static String url = "https://www.selenium.dev/";
+
+    static {
+        options = new ChromeOptions();
+        options.addArguments("--disable-notifiactions");
+        driver = new ChromeDriver(options);
+        actions = new Actions(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public static void main(String[] args) {
+        try{
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        }catch (Exception ignore){
+            throw new RuntimeException(ignore.getMessage());
+        }
+        driver.get(url);
+        contextClickOnScreen();
+        try{Thread.sleep(5000);}catch (Exception ignore){}
+        WebElement element = driver.findElement(By.id("Layer_1"));
+        contextClickOnElement(element);
+        try{Thread.sleep(5000);}catch (Exception ignore){}
+    }
+
+    /**
+     * This method is used to perform the context click on screen.
+     */
+    public static void contextClickOnScreen(){
+        actions.contextClick().build().perform();
+    }
+
+    /**
+     * This method is used to perform the context click on element.
+     * @param element <code>{@link WebElement}</code>
+     */
+    public static void contextClickOnElement(WebElement element){
+        actions.contextClick(element).build().perform();
+    }
 }
